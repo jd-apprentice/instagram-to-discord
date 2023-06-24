04045794dacc03cedcc83dacbfece9e7a2858a2a
Jonathan Dyallo <emeraldusk@gmail.com>
2023-06-24 00:48:12 -0300

    fix(script): deleting in both cases

--- utils/fetch_posts.sh
+++ utils/fetch_posts.sh
@@ -9,6 +9,16 @@ fi
 
 if [ ! -d "./$username" ]; then
     timeout 10s instaloader "$username" --no-captions
+
+    files_without_date=$(ls -1tr ./$username/*.json.xz | grep -v "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}")
+
+    if [ -n "$files_without_date" ]; then
+        rm $files_without_date
+    fi
+
+    find ./$username -type f \( -name "*.txt" -o -name "*.jpg" -o -name "*.mp4" \) -delete
+    rm ./$username/id
+
     exit 1
 fi
 
